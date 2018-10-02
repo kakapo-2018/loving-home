89be78c2aa53def770955e832f9891348d5914d8
dev-welli <welli.abdullah@xero.com>
2018-10-03 09:37:38 +1300

    a bit cleaning up storescreen

--- screens/StoreScreen.js
+++ screens/StoreScreen.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react'
 import {
       StyleSheet,
-      View, Image,
+      View,
       FlatList,
       Text,
-      Animated,
       ImageBackground,
-      TouchableOpacity,
       TouchableWithoutFeedback
 } from 'react-native'
 
@@ -17,27 +15,12 @@ import { fetchCosmetics, updateActiveStoreCarousel, setAllCosmetics, spendMoney 
 
 class TestScreen extends Component {
       state = {
-            columns:3,
-            animatePress: new Animated.Value(1)
+            columns:3
       }
 
       static navigationOptions = {
             header: null,
           };
-
-      animateIn(){
-            Animated.timing(this.state.animatePress,{
-                  toValue:0.8,
-                  duration:400
-            }).start()
-      }
-
-      animateOut(){
-            Animated.timing(this.state.animatePress,{
-                  toValue:1,
-                  duration:400
-            }).start()
-      }
 
       componentDidMount(){
             this.props.fetchCosmetics()
@@ -54,20 +37,8 @@ class TestScreen extends Component {
                   numColumns = {columns}
                    data = {this.props.storeItems.storeCarousel.cosmetics}
                     renderItem = {({ item }) => {
-                        return <TouchableWithoutFeedback 
-                              onPressIn={() => {this.props.spendMoney(item.price); this.animateIn()}} 
-                              onPressOut = {()=>this.animateOut()}>
-                              <Animated.View style = {{margin: 5, width:150, height:100, backgroundColor: 'tomato',
-                                    transform:[
-                                        {
-                                              scale:this.state.animatePress
-                                        }  
-                                    ]
-                        }}>
-                              <Image style = {{width:150, height:75}} source={require('../assets/images/cat.png')}/>
-                              <Text>{item.name} ${item.price}</Text>
-                              </Animated.View>
-                              </TouchableWithoutFeedback>
+                        return <TouchableWithoutFeedback onPress={() => this.props.spendMoney(item.price)}>
+                        <ReUse  name={item.name} price={item.price} image={'./assets/images/cat.png'}/></TouchableWithoutFeedback>
                   }}
                     keyExtractor = {
                           (index) => {return index}
@@ -118,17 +89,3 @@ const styles = StyleSheet.create({
             
       }
 })
-
-
-
-// [
-//   require('../assets/images/cat.png'),
-//   require('../assets/images/dog.png'),
-//   require('../assets/images/robot-dev.png'),
-//   require('../assets/images/neko-atsume.jpg'),
-//   require('../assets/images/robot-prod.png'),
-//   require('../assets/images/store_background.jpg'),
-//   require('../assets/images/cat.png'),
-//   require('../assets/images/robot-dev.png'),
-//   require('../assets/images/robot-prod.png')
-// ]
